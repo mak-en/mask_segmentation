fc36ffe1479bb8985112638b62765e2bc212b523
gladiolusnomertri <makoveev90@gmail.com>
2022-01-18 15:19:35 +0100

    dataset

--- train_mask.py
+++ train_mask.py
@@ -1,6 +1,8 @@
 # Imports
-from tokenize import String
-from typing import List
+import os
+
+import numpy as np
+from PIL import Image
 from torch.utils.data import DataLoader, Dataset
 
 # Dataset
@@ -10,59 +12,30 @@ class CovMask(Dataset):
     The link - https://www.kaggle.com/perke986/face-mask-segmentation-dataset
     """
 
-    def __init__(
-        self,
-        data_path: String,
-        split: List,
-        transform=None
-    ):
+    def __init__(self, data_path, transform=None):
 
         self.transform = transform
 
-        self.split = split
         self.data_path = data_path
-        self.img_path = os.path.join(self.data_path, 'training/image_2')
-        self.mask_path = os.path.join(self.data_path, 'training/semantic')
+        self.img_path = os.path.join(self.data_path,'data_mask/images')
+        self.mask_path = os.path.join(self.data_path, 'data_mask/masks')
         self.img_list = self.get_filenames(self.img_path)
-        self.mask_list = self.get_filenames(self.mask_path)
-
-        # Split between train and valid set
-        random_inst = random.Random(12345)  # for repeatability
-        n_items = len(self.img_list)
-        idxs = random_inst.sample(range(n_items), n_items // 5)
-        if self.split == 'train': idxs = [idx for idx in range(n_items) if idx not in idxs]
-        self.img_list = [self.img_list[i] for i in idxs]
-        self.mask_list = [self.mask_list[i] for i in idxs]        
+        self.mask_list = self.get_filenames(self.mask_path)    
 
     def __len__(self):
         return(len(self.img_list))
 
     def __getitem__(self, idx):
         img = Image.open(self.img_list[idx])
-        img = img.resize(self.img_size)
         img = np.array(img)
 
-        mask = Image.open(self.mask_list[idx]).convert('L')
-        mask = mask.resize(self.img_size)
+        mask = Image.open(self.mask_list[idx])
         mask = np.array(mask)
-        mask = self.encode_segmap(mask)
 
         if self.transform:
             img = self.transform(img)
 
         return img, mask
-
-    def encode_segmap(self, mask):
-        '''
-        Sets void classes to zero so they won't be considered for training
-        '''
-        for voidc in self.void_labels:
-            mask[mask == voidc] = self.ignore_index
-        for validc in self.valid_labels:
-            mask[mask == validc] = self.class_map[validc]
-        # remove extra idxs from updated dataset
-        mask[mask>18]=self.ignore_index
-        return mask
 
     def get_filenames(self, path):
         '''
@@ -71,4 +44,22 @@ class CovMask(Dataset):
         files_list = list()
         for filename in os.listdir(path):
             files_list.append(os.path.join(path, filename))
-        return files_list 
+        return files_list
+    
+    def show_example(self, idx):
+        '''
+        Demonstrates an 'idx' instance of the dataset.
+        If there is self.transformation, shows with the applied transformation
+        '''
+        img = Image.open(self.img_list[idx])
+        mask = Image.open(self.mask_list[idx])
+
+        if self.transform:
+            img = self.transform(img)
+
+        img.show()
+        mask.show()
+
+if __name__ == '__main__':
+    sd = CovMask('C:/Users/makov/Desktop')
+    sd.show_example(10)
