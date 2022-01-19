dfc7893581f8cebf20be5e6dd4fb1ad0ffb97af4
gladiolusnomertri <makoveev90@gmail.com>
2022-01-19 14:59:37 +0100

    pl DataModule

--- train_mask.py
+++ train_mask.py
@@ -4,6 +4,22 @@ import os
 import numpy as np
 from PIL import Image
 from torch.utils.data import DataLoader, Dataset
+import pytorch_lightning as pl
+import torchvision.transforms as transforms
+import torch
+import matplotlib.pyplot as plt
+
+# Sweep initial parameters
+hyperparameter_defaults = dict(
+    data_path='data_semantics',
+    batch_size = 2,
+    lr = 1e-3,
+    num_layers = 5,
+    features_start = 64,
+    bilinear = False,
+    grad_batches = 1,
+    epochs = 20
+)
 
 # Dataset
 class CovMask(Dataset):
@@ -12,9 +28,7 @@ class CovMask(Dataset):
     The link - https://www.kaggle.com/perke986/face-mask-segmentation-dataset
     """
 
-    def __init__(self, data_path, transform=None):
-
-        self.transform = transform
+    def __init__(self, data_path):
 
         self.data_path = data_path
         self.img_path = os.path.join(self.data_path,'data_mask/images')
@@ -31,9 +45,6 @@ class CovMask(Dataset):
 
         mask = Image.open(self.mask_list[idx])
         mask = np.array(mask)
-
-        if self.transform:
-            img = self.transform(img)
 
         return img, mask
 
@@ -54,12 +65,91 @@ class CovMask(Dataset):
         img = Image.open(self.img_list[idx])
         mask = Image.open(self.mask_list[idx])
 
-        if self.transform:
-            img = self.transform(img)
-
         img.show()
         mask.show()
 
+class MaskDataset(pl.LightningDataModule):
+    '''
+    Pytorch Lightning DataModule.
+    Moving the transform operation here (from the CovMask(Dataset)) allows
+    a flexible adjustment of the train, validate and test subsets.
+    '''
+
+    def __init__(self, data_path, transform=None):
+        super().__init__()
+        # print(hparams)
+        self.data_path = data_path
+        self.batch_size = 1
+
+        # Transforms for train, val, test subsets (can be different)
+        if transform:
+            self.train_transform = transform
+            self.val_transform = transform
+            self.test_transform = transform
+        else:
+            pass
+        # self.transform = transforms.Compose([
+        #     transforms.ToTensor(),
+        #     transforms.Normalize(mean=[0.35675976, 0.37380189, 0.3764753],
+        #                          std=[0.32064945, 0.32098866, 0.32325324])
+        # ])
+    
+    def setup(self):
+        dataset = CovMask(self.data_path)
+        train_size = int(0.7 * len(dataset)) # take 70% for training
+        val_size = int(0.2 * len(dataset)) # take 20% for validation
+        test_size = len(dataset) - (train_size + val_size) # take 10% for test
+
+        self.train_set, self.val_set, self.test_set = \
+        torch.utils.data.random_split(dataset, 
+                                      [train_size, val_size, test_size])
+
+        self.train_set.dataset.transform = self.train_transform
+        self.val_set.dataset.transform = self.val_transform
+        self.test_set.dataset.transform = self.test_transform
+    
+    def train_dataloader(self):
+        return DataLoader(self.train_set, batch_size=self.batch_size,
+                          shuffle=True)
+
+    def val_dataloader(self):
+        return DataLoader(self.val_set, batch_size=self.batch_size,
+                          shuffle=False)
+
+    def visualize_dataset(self):
+        # Visualizes a piece of the train subset
+        figure = plt.figure(figsize=(8, 8))
+        cols, rows = 3, 3
+        for i in range(1, cols * rows + 1):
+            sample_idx = torch.randint(len(self.train_set), size=(1,)).item()
+            norm_img, mask = self.train_set[sample_idx]
+            # mean = torch.tensor([0.485, 0.456, 0.406])
+            # std = torch.tensor([0.229, 0.224, 0.225])
+            # img = norm_img * std[:, None, None] + mean[:, None, None] 
+            figure.add_subplot(rows, cols, i)
+            # plt.title(self.labels_map[label])
+            plt.axis("off")
+            # plt.imshow(norm_img.permute(1, 2, 0))
+            plt.imshow(norm_img)
+        plt.show()
+
+    def visualize_dataloader(self):
+        # Display one image and label from a train-subset batch
+        train_dataloader = self.train_dataloader()
+        train_features, train_labels = next(iter(train_dataloader))
+        print(f"Feature batch shape: {train_features.size()}")
+        print(f"Labels batch shape: {train_labels.size()}")
+        norm_img = train_features[0]
+        # mean = torch.tensor([0.485, 0.456, 0.406])
+        # std = torch.tensor([0.229, 0.224, 0.225])
+        # img = norm_img * std[:, None, None] + mean[:, None, None]
+        mask = train_labels[0]
+        # plt.imshow(img.permute(1, 2, 0))
+        plt.imshow(mask)
+        plt.show()
+        # print(f"Label: {self.labels_map[label.item()]}")
+
 if __name__ == '__main__':
-    sd = CovMask('C:/Users/makov/Desktop')
-    sd.show_example(10)
+    sd = MaskDataset('C:/Users/makov/Desktop')
+    sd.setup()
+    sd.visualize_dataloader()
