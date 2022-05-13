b0a735ffc848e6a4509b2df3efbe2786f702c3f7
gladiolusnomertri <makoveev90@gmail.com>
2022-05-13 19:00:46 +0200

    prepare_dataset.py, comments how to use have been added

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -1,9 +1,49 @@
 import splitfolders
 
+# Split folders with files (e.g. images) into train, validation and
+# test (dataset) folders.
+#
+# The input folder should have the following format:
+#
+# input/
+#     class1/
+#         img1.jpg
+#         img2.jpg
+#         ...
+#     class2/
+#         imgWhatever.jpg
+#         ...
+#     ...
+# In order to give you this:
+#
+# output/
+#     train/
+#         class1/
+#             img1.jpg
+#             ...
+#         class2/
+#             imga.jpg
+#             ...
+#     val/
+#         class1/
+#             img2.jpg
+#             ...
+#         class2/
+#             imgb.jpg
+#             ...
+#     test/
+#         class1/
+#             img3.jpg
+#             ...
+#         class2/
+#             imgc.jpg
+#             ...
+
 # Split with a ratio.
 # To only split into training and validation set, set a tuple to `ratio`,
 # i.e, `(.8, .2)`.
 # Train, val, test
+
 splitfolders.ratio(
     "C:/Users/ant_on/Desktop/data_mask/",
     output="./data",
